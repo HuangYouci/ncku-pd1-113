d7132636ef054eb5ff7a5690ad1ee0484b171f91
HuangYouci <79450931+HuangYouci@users.noreply.github.com>
2024-11-12 10:10:41 +0800

    優化程式碼以使城市執行效率更高

--- HW5/pD.c
+++ HW5/pD.c
@@ -1,29 +1,38 @@
 #include <stdio.h>
 
 int main(){
-    int n;
+    int n = 0, np = 0;
+
     scanf("%d", &n);
 
-    int a[n];
+    long long a[n], b[n+1];
     for(int i = 0; i < n ; i++){
-        scanf("%d", &a[i]);
+        scanf("%lld", &a[i]);
+        b[i+1] = b[i] + a[i];
     }
 
     int q;
     scanf("%d", &q);
 
-    int l, r, total, answerarray[q];
+    int l, r, total;
+    long long answerarray[q];
     for(int i = 0; i < q ; i++){
-        total = 0;
-        scanf("%d %d", &l, &r);
-        for(int j = l; j <= r; j++){
-            total += a[j-1];
-        }
-        answerarray[i] = total;
+        scanf("%lld %lld", &l, &r);
+        answerarray[i] = (b[r] - b[l-1]);
     }
 
+    // for(int i = 0 ; i < (n+1) ; i++){
+    //     printf("[%d] ", a[i]);
+    // }
+
+    // printf("\n");
+
+    // for(int i = 0 ; i < (n+1) ; i++){
+    //     printf("[%d] ", b[i]);
+    // }
+
     for(int i = 0; i < q; i++){
-        printf("%d\n", answerarray[i]);
+        printf("%lld\n", answerarray[i]);
     }
 
     return 0;
